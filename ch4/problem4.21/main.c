e06fcddaa16ad6a626ee8f16cdf2f9df1c8b2e06
Sheldon Zhong <11510383@mail.sustc.edu.cn>
2018-12-27 20:14:49 +0800

    finish problem4.21

--- ch4/problem4.21/main.c
+++ ch4/problem4.21/main.c
@@ -3,53 +3,80 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
+#include <pthread.h>
 
 #define MAX_LENGTH 100
 
-int maxFunc(int arr[MAX_LENGTH], int end)
+struct prog_param
 {
-    int max = INT_MIN;
+    int *arr;
+    int end;
+};
+
+int avg = 0;
+int min = 0;
+int max = 0;
+
+void *maxFunc(void *param)
+{
+    struct prog_param *s = param;
+    int end = s->end;
+    int maxl = INT_MIN;
     for (int i = 0; i < end; i++)
     {
-        if (max < arr[i])
+        if (maxl < s->arr[i])
         {
-            max = arr[i];
+            maxl = s->arr[i];
         }
     }
 
-    return max;
+    max = maxl;
+    pthread_exit(0);
 }
 
-int minFunc(int arr[MAX_LENGTH], int end)
+void *minFunc(void *param)
 {
-    int min = INT_MAX;
+    struct prog_param *s = param;
+    int end = s->end;
+    int minl = INT_MAX;
+
     for (int i = 0; i < end; i++)
     {
-        if (min > arr[i])
+        if (minl > s->arr[i])
         {
-            min = arr[i];
+            minl = s->arr[i];
         }
     }
 
-    return min;
+    min = minl;
+    pthread_exit(0);
 }
 
-int avgFunc(int arr[MAX_LENGTH], int end)
+void *avgFunc(void *param)
 {
+    struct prog_param *s = param;
+    int end = s->end;
+
     int sum = 0;
     for (int i = 0; i < end; i++)
     {
-        sum += arr[i];
+        sum += s->arr[i];
     }
 
-    return sum / end;
+    avg = sum / end;
+    pthread_exit(0);
 }
 
 int main(int argc, char *argv[])
 {
     char *arr;
     int buffer[MAX_LENGTH];
-    pid_t pid;
+    pthread_t tid_avg;
+    pthread_t tid_min;
+    pthread_t tid_max;
+
+    pthread_attr_t attr;
+
     int temp = 0;
     if (argc > 1)
     {
@@ -63,14 +90,18 @@ int main(int argc, char *argv[])
     {
         return 1;
     }
+    struct prog_param param = {&buffer[0], argc - 1};
 
-    int avg = 0;
-    int min = 0;
-    int max = 0;
+    // get thread attributes
+    pthread_attr_init(&attr);
 
-    avg = avgFunc(buffer, argc - 1);
-    min = minFunc(buffer, argc - 1);
-    max = maxFunc(buffer, argc - 1);
+    // create a thread
+    pthread_create(&tid_avg, &attr, avgFunc, &param);
+    pthread_create(&tid_min, &attr, minFunc, &param);
+    pthread_create(&tid_max, &attr, maxFunc, &param);
+    pthread_join(tid_avg, NULL);
+    pthread_join(tid_min, NULL);
+    pthread_join(tid_max, NULL);
 
     printf("The average value is %d\n", avg);
     printf("The minimum value is %d\n", min);
