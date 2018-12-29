23b307d87e5f62855f61d11095cdc1aa7b9b4203
Sheldon Zhong <11510383@mail.sustc.edu.cn>
2018-12-29 08:33:50 -0800

    most efficient primes, in terms of time complexity

--- ch4/problem4.24/main.c
+++ ch4/problem4.24/main.c
@@ -10,26 +10,33 @@
 // Maximum input: 4096 * 32 = 131072
 u_int32_t buffer[BUFFER_SIZE];
 const int op = 1;
-
+int cnt = 0;
 void *primes(void *param)
 {
     int *p = param;
     int n = *p;
     int shift, idx, tmp, prime;
+    // both factors should less than or equal to sqrt(n)
     for (int i = 2; i * i <= n; i++)
     {
         idx = i / 32;
         shift = i % 32;
         prime = (!(buffer[idx] & (op << shift)));
-        for (int j = 2; prime && (tmp <= n); j++)
+        // ternaory operator to skip prime increment factor of 2
+        // actually we could skip all non-prime factors, but we lost the information
+        for (int j = i * i; prime && (j <= n); j += ((i > 2 ? 2 : 1) * i))
         {
+            // i is prime, i * i, i * (i + 1), ...
+            // because 1 * i, 2 * i, ...., (i - 1) * i, have been calculated
+            // 4, 6, 8, ....
+            // 9, 12(skipped), 15, 18(skipped), ...
+            // 25, 30(skipped), 35, 40(skipped), 45(should skipped for 3), 50(skipped), ...
             // multiple of primes
-            tmp = i * j;
-            shift = tmp % 32;
-            idx = tmp / 32;
+            shift = j % 32;
+            idx = j / 32;
             buffer[idx] |= (op << shift);
+            cnt++;
         }
-        tmp = 0;
     }
 
     pthread_exit(0);
