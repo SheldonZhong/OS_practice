ca097d67ff143c73bbf900a361734e26183eb5a1
Sheldon Zhong <11510383@mail.sustc.edu.cn>
2018-12-28 19:42:18 -0800

    add multreaded Monte Carlo pi calculation

---
+++ ch4/problem4.22/main.c
@@ -0,0 +1,51 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <math.h>
+
+int hits = 0;
+
+double frand()
+{
+    return (double)rand() / (double)RAND_MAX;
+}
+
+void *cal_pi(void *param)
+{
+    int total = atoi(param);
+    double x, y;
+    for (int i = 0; i < total; i++)
+    {
+        x = frand();
+        y = frand();
+
+        if (sqrt(x * x + y * y) < 1)
+        {
+            hits++;
+        }
+    }
+    pthread_exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    pthread_t tid;
+    pthread_attr_t attr;
+
+    // get thread attributes
+    pthread_attr_init(&attr);
+
+    // create a thread
+    pthread_create(&tid, &attr, cal_pi, argv[1]);
+
+    // wait for each thread to finish
+    pthread_join(tid, NULL);
+    int total = atoi(argv[1]);
+
+    printf("Monte Carlo Pi: %f\n", 4 * (double)hits / (double)total);
+}
