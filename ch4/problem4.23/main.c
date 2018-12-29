aab984b75d38cb5ede014ca8452eec0d272d5474
Sheldon Zhong <11510383@mail.sustc.edu.cn>
2018-12-29 01:53:26 -0800

    remove comments

--- ch4/problem4.23/main.c
+++ ch4/problem4.23/main.c
@@ -1,17 +1,3 @@
-/**
- * Example of openmp parallel region
- * 
- * To compile, enter:
- *
- *	gcc -fopenmp openmp.c
- *
- * You should see the message "I am a parallel region" for each
- * processing core on your system.
- *
- * For those using a virtual machine, make sure you set the number of
- * processing cores > 1 to see parallel execution of the parallel region.
- */
-
 #include <omp.h>
 #include <stdio.h>
 #include <unistd.h>
