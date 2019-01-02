591b7a13ee4c96819ca24c522168d7ae54fafe89
Sheldon Zhong <11510383@mail.sustc.edu.cn>
2019-01-02 11:05:46 +0800

    multithreaded dateServer

--- ch4/problem4.25/DateServer.java
+++ ch4/problem4.25/DateServer.java
@@ -8,13 +8,38 @@
  * Operating System Concepts  - Ninth Edition
  * Copyright John Wiley & Sons - 2013.
  */
- 
+
+// Modify the socket-based date server (Figure 3.21) in Chapter 3 so that
+// the server services each client request in a separate thread.
+
 import java.net.*;
 import java.io.*;
 
-public class DateServer
-{
-	public static void main(String[] args)  {
+class Reply implements Runnable {
+	private Socket client;
+
+	public Reply(Socket client) {
+		this.client = client;
+	}
+
+	public void run() {
+		try {
+			System.out.println("Thread started");
+			PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
+			// write the Date to the socket
+			pout.println(new java.util.Date().toString());
+
+			// close the socket and resume listening for more connections
+			client.close();
+} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+}
+
+public class DateServer {
+	public static void main(String[] args) {
 		try {
 			ServerSocket sock = new ServerSocket(6013);
 
@@ -22,17 +47,11 @@ public class DateServer
 			while (true) {
 				Socket client = sock.accept();
 				// we have a connection
-				
-				PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
-				// write the Date to the socket
-				pout.println(new java.util.Date().toString());
-
-				// close the socket and resume listening for more connections
-				client.close();
+				Thread thrd = new Thread(new Reply(client));
+				thrd.start();
 			}
-		}
-		catch (IOException ioe) {
-				System.err.println(ioe);
+		} catch (IOException ioe) {
+			System.err.println(ioe);
 		}
 	}
 }
