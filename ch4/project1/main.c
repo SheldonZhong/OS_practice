d0dd7d80df965a6c387e9523dbfb01afea936571
Sheldon Zhong <11510383@mail.sustc.edu.cn>
2019-01-03 09:07:27 +0800

    sudoku verification

--- ch4/project1/main.c
+++ ch4/project1/main.c
@@ -6,12 +6,6 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-// #define ROW 0
-// #define COLUMN 1
-// #define DIAGONAL1 2
-// #define DIAGONAL2 3
-// #define GRID 4
-
 typedef enum
 {
     ROW,
@@ -22,7 +16,6 @@ typedef enum
 } Valid_Type;
 
 char sudoku[9][9];
-// char row[9];
 int mask = 0x03FE;
 int results[29];
 
@@ -43,83 +36,57 @@ void *valid_worker(void *param)
     switch (params->type)
     {
     case ROW:
-        printf("ROW\n");
         for (char *p = &sudoku[params->column][0]; p != &sudoku[params->column][9]; p++)
         {
             result |= (1 << *p);
-            printf("%d ", *p);
         }
-        printf("\n");
         results[params->column] = result;
         break;
 
     case COLUMN:
-        printf("COLUMN\n");
         for (int i = 0; i < 9; i++)
         {
             row = &sudoku[i][0];
             cell = row[params->row];
             result |= (1 << cell);
-            printf("%d ", cell);
         }
-        printf("\n");
         results[params->row + 9] = result;
         break;
 
     case DIAGONAL1:
-        printf("DIAGONAL1\n");
         for (int i = 0; i < 9; i++)
         {
             row = &sudoku[i][0];
             cell = row[i];
             result |= (1 << cell);
-            printf("%d ", cell);
         }
-        printf("\n");
         results[27] = result;
         break;
 
     case DIAGONAL2:
-        printf("DIAGONAL2\n");
         for (int i = 0; i < 9; i++)
         {
             row = &sudoku[i][0];
             cell = row[8 - i];
             result |= (1 << cell);
-            printf("%d ", cell);
         }
-        printf("\n");
         results[28] = result;
         break;
 
     case GRID:
-        printf("GRID\n");
         row = &sudoku[params->column][0];
         for (int i = 0; i < 9; i++)
         {
             row = &sudoku[params->column + i / 3][0];
             cell = row[params->row + i % 3];
             result |= (1 << cell);
-            printf("%d ", cell);
         }
-        printf("\n");
-        // (0, 0) -> 0
-        // (0, 1) -> 1
-        // (0, 2) -> 2
-        // (1, 0) -> 3
-        // (1, 1) -> 4
-        // (1, 2) -> 5
-        // (2, 0) -> 6
-        // (2, 1) -> 7
-        // (2, 2) -> 8
         results[((params->column / 3) * 3) + params->row / 3 + 18] = result;
         break;
 
     default:
         break;
     }
-
-    printf("True %d\n", result);
 
     pthread_exit(0);
 }
@@ -132,7 +99,6 @@ int main(int argc, char *argv[])
     Parameters *params[29];
     Parameters *param;
 
-    // Parameters *params = malloc(sizeof(Parameters));
     for (int i = 0; i < 9; i++)
     {
         param = malloc(sizeof(Parameters));
@@ -188,16 +154,11 @@ int main(int argc, char *argv[])
 
     pthread_attr_init(&attr);
 
-    // for (int i = 0; i < 29; i++)
-    //     pthread_create(&tid[i], &attr, valid_worker, params[i]);
-
-    // for (int i = 0; i < 29; i++)
-    //     pthread_join(tid[i], NULL);
     for (int i = 0; i < 29; i++)
-    {
         pthread_create(&tid[i], &attr, valid_worker, params[i]);
+
+    for (int i = 0; i < 29; i++)
         pthread_join(tid[i], NULL);
-    }
 
     for (int i = 0; i < 9; i++)
     {
@@ -212,8 +173,9 @@ int main(int argc, char *argv[])
     {
         if (results[i] != mask)
         {
-            printf("Not valid\n");
-            // break;
+            printf("Not a valid sudoku\n");
+            return 1;
         }
     }
+    printf("Valid sudoku\n");
 }
