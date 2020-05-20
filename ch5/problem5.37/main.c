00f50c43ed50de9c38656939397d7ba209a7f5de
Wenshao Zhong <jzhong@foxmail.com>
2020-05-20 17:32:09 -0500

    finish problem 5.37

--- ch5/problem5.37/main.c
+++ ch5/problem5.37/main.c
@@ -5,6 +5,8 @@
 // available_resources would be involved in the race condition
 int available_resources = MAX_RESOURCES;
 
+sem_t sem;
+
 /*
 decrease available_resources by count resources
 return 0 if sufficient resources available,
@@ -12,31 +14,40 @@ otherwise return -1
 */
 int decrease_count(int count)
 {
-    if (available_resources < count)
-        return -1;
-    else
-    {
-        available_resources -= count;
+    int ret = 0;
 
-        return 0;
+    sem_wait(&sem);
+    if (available_resources < count) {
+        ret = -1;
+    } else {
+        available_resources -= count;
+        ret = 0;
     }
+    sem_post(&sem);
+    return ret;
 }
 
 // increase available_resources by count
 int increase_count(int count)
 {
+    sem_wait(&sem);
     available_resources += count;
+    sem_post(&sem);
 
     return 0;
 }
 
 int main(void)
 {
-    sem_t sem;
-
     sem_init(&sem, 0, 1);
 
-    sem_wait(&sem);
+    // get resource
+    int count = 1;
+    while (decrease_count(count) == -1);
+    
+    // release resource
+    count = 1;
+    increase_count(count);
 
-    sem_post(&sem);
+    sem_destroy(&sem);
 }
